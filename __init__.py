1a1f40adc57e9f5679b220f162f6e834c94f2a64
Hilicot <42719353+Hilicot@users.noreply.github.com>
2019-09-17 10:16:50 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -26,7 +26,7 @@ bl_info = {
     "location": "UV/Image Editor > Properties > Image",
     "description": "Estimates the time to complete rendering on animations",
     "warning": "Does not work on OpenGL renders.",
-    "wiki_url": "http://wiki.blender.org/index.php/Extensions:2.6/Py/Scripts/Render/Render_Time_Estimation",
+    "wiki_url": "https://github.com/Hilicot/Blender-2.8-Render-Time-Extimation",
     "tracker_url": "http://projects.blender.org/tracker/index.php?func=detail&aid=30452&group_id=153&atid=467",
     "category": "Render"}
 
